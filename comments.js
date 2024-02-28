270f7feeec35cca5c73b66b34eded944176b9255
aumsp17 <aumsp17@gmail.com>
2024-02-28 19:35:07 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,36 @@
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsFile = path.join(__dirname, 'comments.json');
+
+// Use body-parser to parse incoming data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+// GET request for comments
+app.get('/api/comments', function(req, res) {
+  fs.readFile(commentsFile, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// POST request for comments
+app.post('/api/comments', function(req, res) {
+  fs.readFile(commentsFile, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    var newComment = {
+      id: Date.now(),
